de4668e8d8ee28d8346e760ae42e486364f679a5
Edsleite <39807952+Edsleite@users.noreply.github.com>
2024-04-24 14:01:52 -0300

    Add files via upload

---
+++ streamlit_ollama_test.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import ollama
+
+
+def generate_response():
+    response=ollama.chat(model="llama3", request_timeout=300.0 , stream=True, message=st.session_state.messages)
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        st.session_state["full_message"] += token
+        yield token
+
+
+st.title("Teste ChatBot COREGPT")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content" : "Como posso lhe ajudar nobre Padawan ?"}]
+
+# Write message history
+for msg in st.session_state.messages:
+    if msg["role"] == "user":
+        st.chat_message(msg["role"] ).write(msg["content"])
+    else:
+        st.chat_message(msg["role"] ).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role" : "user", "content" : prompt})
+    st.chat_message("user").write(prompt)
+    st.session_state["full_message"] = ""
+    st.chat_message("assistant").write_stream(generate_response)
+    st.session_state["full_message"] = ""
+    st.session_state.messages.append({"role" : "assistant", "content" : st.session_state["full_message"]})
